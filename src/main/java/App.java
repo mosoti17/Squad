441e698c4e8899f1aa710910fb7dbca6da96892c
mosoti17 <mosoti17@gmail.com>
2017-08-20 20:31:00 +0300

    adde procfile

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,17 @@ import java.util.ArrayList;
 public class App {
 
     public static void main(String[] args) {
-      staticFileLocation("/public");
+      ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
 
+    setPort(port);
+
+      staticFileLocation("/public");
       String layout = "templates/layout.vtl";
 
       get("/", (request, response) -> {
